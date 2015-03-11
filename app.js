fe52b88ef7f4232960f0c4b2051cdf9e1c18fe0e
Miguel Alvarez <mig.alvarez@yahoo.es>
2015-03-11 15:32:20 -0400

    initial commit: server Up and running

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require( 'express' );
+var app = express();
+
+var morgan = require('morgan');
+app.use(morgan('dev'));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world')
+})
