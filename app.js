bf6bb411a704ed5a555d4d9bd775d98bf5399c2b
Miguel Alvarez <mig.alvarez@yahoo.es>
2015-03-11 17:03:02 -0400

    TweetBank visible

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require( 'express' );
 var app = express();
 var swig = require('swig');
+var routes = require('./routes/');
 swig.setDefaults({ cache: false });
 
 
 var morgan = require('morgan');
 app.use(morgan('dev'));
+app.use('/', routes);
 
 var server = app.listen(3000, function () {
 
@@ -18,16 +20,16 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+app.use(express.static(__dirname + '/public'));
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-	res.render( 'index', {title: 'Hall of Shame', people: people} );
-  	//res.send('hello world')
-})
-
-app.get('/news', function(req, res) {
-	res.send('news!');
-})
+// app.get('/', function(req, res) {
+// 	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// 	res.render( 'index', {title: 'Hall of Shame', people: people} );
+//   	//res.send('hello world')
+// })
+// app.get('/news', function(req, res) {
+// 	res.send('news!');
+// })
 
 
 
